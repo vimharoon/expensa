caa8f82adef24a1b970de7c7b30292ab5e64cc78
mac <mac@mac.local>
2019-04-17 00:34:04 +0200

    init new nodejs server with express and add travisci yml

---
+++ server.js
@@ -0,0 +1,31 @@
+'use strict';  // /!\  -i max for pm2
+const express = require('express');
+const app = express();
+const helmet = require('helmet');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+// this will disable etag header for all requests
+app.disable('etag').disable('x-powered-by');
+// enable .env files
+require('dotenv').config();
+
+// use helmet for secure apps by setting various HTTP headers
+app.use(helmet());
+
+// use morgan to show the logs in console
+app.use(morgan('dev'));
+
+app.use('/website', express.static('public'));
+
+app.get('/', (req, res) => {
+  res.send('ci with travis');
+});
+
+// use body parser middlewares to parse request
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.listen(process.env.PORT || 8000);
+// console.log(`server running on port http://localhost:8000`);
+
+module.exports = app;
