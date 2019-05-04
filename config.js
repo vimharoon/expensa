1a60641e9d3e61c52641a3d524fc681ae3356106
mac <mac@mac.local>
2019-05-04 21:19:20 +0200

    😁

--- config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-  mysqlURL: process.env.MYSQL_URL || 'localhost',
-  port: process.env.PORT || 8000,
-  dbName: 'test',
-};
-
-export default config;
--- tests/server.spec.js
+++
@@ -1,8 +0,0 @@
-const expect = require('chai').expect;
-const server = require('./../server');
-
-describe('server running test', () => {
-  it('should return a string', () => {
-    expect('ci with cercleci').to.equal('ci with cercleci');
-  });
-});
